0ea912773ae69ea21364fc9b921e53b4f1d90489
graef <simonjn45@gmail.com>
2024-01-28 18:03:55 +0100

    Fixed Mobile Layout(hopefully)

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 // import json from '../json/languages.json' assert { type: 'json' };
 // const languageArray = json;
 
-const languageArray = await(await fetch("./json/languages.json")).json();
+const languageArray = await (await fetch("./json/languages.json")).json();
 
 const language_list = document.getElementById("language-list");
 languageArray.sort((a, b) => a.name.localeCompare(b.name));
@@ -77,7 +77,12 @@ const createRegister = (e) => {
     interpreters += "No Interpreters.";
   }
 
-  const link = `https://github.com/topics/${e.name.replaceAll("++", "pp").replaceAll("#", "sharp").replaceAll("*", "star")}`;
+  const link = `https://github.com/topics/${
+    e.name.replaceAll("++", "pp").replaceAll("#", "sharp").replaceAll(
+      "*",
+      "star",
+    )
+  }`;
 
   language.innerHTML =
     `<h1 class="language-header" style="color: ${title_fg}; background-color: ${title_bg};" ><a target="_blank" href="${link}">${e.name}</a></h1><table class="language-table"><tbody><tr><td>Hexcode</td><td><code>${title_bg}</code></td></tr><tr><td>Type</td><td><code>${e.type}</code></td></tr><tr><td>Extensions</td><td>${extensions}</td></tr><tr><td>Filenames</td><td>${filenames}</td></tr><tr><td>Interpreters</td><td>${interpreters}</td></tr></tbody></table>`;
